ef774540f85397bbdf78311c58ae3501b8c34fe0
Maciej Kasinski <maciejkasinski@gmail.com>
2017-10-29 20:49:09 +0100

    adjust rectangle to margin/2, reduce code

--- lib/doc_size_distr_plot.R
+++ lib/doc_size_distr_plot.R
@@ -3,7 +3,21 @@ doc_size_distr_plot = function(df = data_niche,
                                build0 = NULL,
                                update = FALSE,
                                suffix = "",
-                               margin = 0.1) {
+                               margin = 0.1,
+                               color = "black") {
+  
+  annotate_rect = function(df_rng, times) {
+    annotate(
+      geom = "rect",
+      xmin = df_rng$min,
+      xmax = df_rng$max,
+      ymin = q0 * times * (1 - margin / 2),
+      ymax = q0 * times * (1 + margin / 2),
+      alpha = 0.3,
+      fill = "blue"
+    )
+  }
+  
   df <- df %>% filter(sector == sector0)
   quantogram = calculate_quantogram(df %>% .$size %>% unround_data)
   q0 = quantogram %>% group_by() %>% filter(f_q == max(f_q)) %>% .$q
@@ -14,9 +28,9 @@ doc_size_distr_plot = function(df = data_niche,
   
   df_plot %>%
     mutate(
-      close_to_q1 = ifelse(abs(q0 - size) / (q0) <= margin, T, F),
-      close_to_q2 = ifelse(abs(q0 * 2 - size) / (q0 * 2) <= margin, T, F),
-      close_to_q3 = ifelse(abs(q0 * 3 - size) / (q0 * 3) <= margin, T, F)
+      close_to_q1 = ifelse(abs(q0 - size) / (q0) <= margin/2, T, F),
+      close_to_q2 = ifelse(abs(q0 * 2 - size) / (q0 * 2) <= margin/2, T, F),
+      close_to_q3 = ifelse(abs(q0 * 3 - size) / (q0 * 3) <= margin/2, T, F)
     ) -> df2
   
   
@@ -27,18 +41,25 @@ doc_size_distr_plot = function(df = data_niche,
   df2 %>% group_by(close_to_q3) %>% summarise(min = min(size), max = max(size)) %>%
     filter(close_to_q3) -> rng3
   
-  filter(df_plot, size >= rng1$min, size <= rng1$max) %>%
-    summarise(min = min(x), max = max(x)) -> x_rng1
+  # if not a single size within 1*quantum margin error
+  if (nrow(rng1) == 0) {
+    x_rng1[1, "min"] <- 0
+    x_rng1[1, "max"] <- 0
+  } else { 
+    filter(df_plot, size >= rng1$min, size <= rng1$max) %>%
+      summarise(min = min(x), max = max(x)) -> x_rng1
+  }
   
-  # possibly 2*quantum not on the plot
+  # if not a single size within 2*quantum margin error
   if (nrow(rng2) == 0) {
     x_rng2[1, "min"] <- 0
     x_rng2[1, "max"] <- 0
   } else { 
     filter(df_plot, size >= rng2$min, size <= rng2$max) %>%
       summarise(min = min(x), max = max(x)) -> x_rng2
-    
   }
+  
+  # if not a single size within 3*quantum margin error
   if (nrow(rng3) == 0) {
     x_rng3[1, "min"] <- 0
     x_rng3[1, "max"] <- 0
@@ -48,55 +69,19 @@ doc_size_distr_plot = function(df = data_niche,
   }
   
   p = ggplot(df_plot, aes(x = x, y = size)) +
-    geom_segment(aes(xend = x, yend = 0), size = 0.3) +
-    scale_x_continuous("") +
+    geom_segment(aes(xend = x, yend = 0), size = 0.3, color=color) +
+    scale_x_continuous(name = "", labels = NULL) +
     scale_y_continuous(name = "Measurement length [m]", limits = c(0, 1.5)) +
     geom_dl(aes(label = sector0),
-            method = list(
-              "last.points",
-              rot = 90,
-              hjust = 1,
-              vjust = 1.5
-            )) +
-    # scale_color_manual(values=cosine_palette_3) +
-    # guides(colour = guide_legend(override.aes = list(size=5))) +
+            method = list("last.points",
+                          rot = 0,
+                          hjust = 1.1,
+                          vjust = 0.5)) +
     guides(colour = "none") +
-    theme_minimal() +
-    theme(
-      plot.title = element_text(hjust = 0.5, size = 15),
-      plot.subtitle = element_text(hjust = 0.5, size = 10),
-      axis.text.x = element_blank(),
-      legend.position = "top"
-    )
-  p +
-    annotate(
-      "rect",
-      xmin = x_rng1$min,
-      xmax = x_rng1$max,
-      ymin = q0 * (1 - margin / 2),
-      ymax = q0 * (1 + margin / 2),
-      alpha = 0.2,
-      color = "blue",
-      fill = "blue"
-    ) +
-    annotate(
-      "rect",
-      xmin = x_rng2$min,
-      xmax = x_rng2$max,
-      ymin = q0 * 2 * (1 - margin / 2),
-      ymax = q0 * 2 * (1 + margin / 2),
-      alpha = 0.2,
-      color = "blue",
-      fill = "blue"
-    ) +
-    annotate(
-      "rect",
-      xmin = x_rng3$min,
-      xmax = x_rng3$max,
-      ymin = q0 * 3 * (1 - margin / 2),
-      ymax = q0 * 3 * (1 + margin / 2),
-      alpha = 0.2,
-      color = "blue",
-      fill = "blue"
-    )
+    theme_minimal()
+  
+  p + 
+    annotate_rect(x_rng1, 1) +
+    annotate_rect(x_rng2, 2) +
+    annotate_rect(x_rng3, 3)
 }
