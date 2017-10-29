ac0f32688c585d905202a195c6557a67aeafd4db
Maciej Kasinski <maciejkasinski@gmail.com>
2017-10-29 19:16:34 +0100

    rename according to existing convention

--- lib/plot_quantogram.R
+++ lib/plot_quantogram.R
@@ -1,4 +1,4 @@
-plotQuantogram = function(df_results, sector_label) {
+plot_quantogram = function(df_results, sector_label) {
   
   names(df_results) <- c("q", "quantogram.score")
   quantum <- df_results %>% 
@@ -25,4 +25,31 @@ plotQuantogram = function(df_results, sector_label) {
             subtitle = subtitle)
   return(sp)
 }
+ 
+plot_quantogram2 = function(df_results, sector_label) {
   
+  names(df_results) <- c("q", "quantogram.score")
+  quantum <- df_results %>% 
+    group_by %>%
+    filter(quantogram.score==max(quantogram.score)) %>% .$q
+  
+  if (quantum %>% length > 1) {
+    message("Recognised multiple candidates for quantum. The highest value was selected")
+    message(paste0(quantum, sep=" "))
+    quantum = max(quantum)
+  }
+  
+  title = sector_label
+  subtitle = paste0("Peak in quantum = ", quantum)
+  
+  sp <- ggplot(df_results, aes(x=q, y=quantogram.score)) + 
+    geom_line() + 
+    geom_point()
+  sp <- sp + 
+    geom_hline(aes(yintercept=0), linetype="dashed") +
+    # geom_vline(xintercept=quantum, colour="#BB0000", linetype="dashed") +
+    theme_minimal() + 
+    ggtitle(label = title, 
+            subtitle = subtitle)
+  return(sp)
+} 
