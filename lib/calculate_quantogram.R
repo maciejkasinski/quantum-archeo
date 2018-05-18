6dca4cb351293c63e14740ffb1d63cc92941abbf
Maciej Kasinski <maciejkasinski@gmail.com>
2018-05-18 22:44:34 +0200

    optimize, add config-dependence and input-assert, return entire results and rename input

--- lib/calculate_quantogram.R
+++ lib/calculate_quantogram.R
@@ -1,24 +1,37 @@
-calculate_quantogram = function(v_input,
-                                qMin = Q_MIN,
-                                rangeStart = RNG_START,
-                                rangeEnd = RNG_END,
-                                step = STEP) {
-  n = v_input %>% length
-  A = sqrt(2 / n)
-  results = NULL
+calculate_quantogram = function(measurements,
+                                params=CONSTANTS_QUANTOGRAM) {
   
-  rng <- seq(rangeStart, rangeEnd, by = step)
-  for (q in rng) {
-    if (q < qMin)
+  if (4 != sum(c("RNG_START", "RNG_END", "STEP", "Q_MIN") %in% names(params))) {
+    print("Incorrect params")
+    return(NULL)
+  }
+  
+  N = length(measurements)
+  A = sqrt(2 / N)
+  
+  
+  results = list()
+  rng = seq(params$RNG_START, params$RNG_END, by = params$STEP)
+  
+  for (n in 1:length(rng)) {
+    q = rng[n]
+    if (q < params$Q_MIN)
       next
     sum = 0
-    for (i in 1:n) {
-      e = v_input[i] %% q
+    for (i in 1:N) {
+      e = measurements[i] %% q
       cosVal = 2.0 * pi * e / q
       sum = sum + cos(cosVal)
     }
     f_q = A * sum # quanta
-    results = rbind(results, data.frame(q, f_q))
+    results[[n]] = c(q, f_q)
   }
+  
+  results = as_tibble(do.call(rbind, results))
+  names(results) = c("q", "f_q")
   return(results)
+}
+
+get_quantum = function(measurements) {
+  calculate_quantogram(measurements) %>% filter(f_q==max(f_q)) %>% pull(q)
 }
